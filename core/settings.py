76245b4eb63416f3bb68ffffac4fad7614588eff
KudaMukucha <kudamukucha80@gmail.com>
2024-11-19 13:05:11 +0200

    debug true for testing

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p_jvs%gnsu*+!)rlu6b(fshyhl+-624ioqk7(yq82-r9e93m!6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost']
 
